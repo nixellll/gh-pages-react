a1b207a5073af071477555809d9f1e152c943f64
Nicolas Castro <nicolas.castro1@coalfire.com>
2022-01-01 22:23:57 -0500

    changes 2

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://nixellll.github.io/',
+  base: 'https://nixellll.github.io/gh-pages-react/',
   plugins: [react()]
 })
